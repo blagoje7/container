bb6acf3d9994beba536794a6aba37f97749a18c4
blagoje7 <stojsavlejvic.blagoje@gmail.com>
2022-04-03 20:59:36 +0200

    prvi commit

---
+++ src/main/java/prvi/kolokvijum/priprema2/Priprema2Application.java
@@ -0,0 +1,13 @@
+package prvi.kolokvijum.priprema2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Priprema2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Priprema2Application.class, args);
+    }
+
+}
---
+++ src/test/java/prvi/kolokvijum/priprema2/Priprema2ApplicationTests.java
@@ -0,0 +1,13 @@
+package prvi.kolokvijum.priprema2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Priprema2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
